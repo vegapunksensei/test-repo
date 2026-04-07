7f93099c7fde24a8244d65a56dd5ad3583ffff9b
Dr. Vega Punk <vegapunksensei@gmail.com>
2026-04-07 09:57:19 +0530

    code v1.1.0: [feat] new lib added
    * Multiplication method added.
    * New lib for maths is added.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include "mlib.h"
 using namespace std;
 
 int main()
 {
 	cout << "Hello World!" << endl;
+	cout << add(2, 4) << endl;
+	cout << sub(6, 2) << endl;
+	cout << mul(6, 2) << endl;
 }
---
+++ mlib.h
@@ -0,0 +1,8 @@
+#ifndef MLIB_H
+#define MLIB_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+
+#endif // MLIB_H
